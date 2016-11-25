fc475ee3e965843fffd2f10d7e950f1a48a30703
fleurgriffon <fleurgriffon@gmail.com>
2016-11-25 17:32:15 +0100

    Second dev pour Twice

--- fleurgriffon.github.io/js/function.js
+++ fleurgriffon.github.io/js/function.js
@@ -35,3 +35,26 @@ $(document).ready(function() {
     });
 
 });
+$(document).ready(function() {
+
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+
+        /* Check the location of each desired element */
+        $('.hidememore').each( function(i){
+
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+
+                $(this).animate({'opacity':'1'},4000);
+
+            }
+
+        });
+
+    });
+
+});
